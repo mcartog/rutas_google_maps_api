e0f6cc9462b0f2eafafbaacbe36a7fe412e60e9e
Unknown <marcostoral@hotmail.com>
2017-04-05 12:01:52 +0200

    Añadida la memoria

---
+++ KeepMoving/app/src/main/java/com/marcostoral/keepmoving/services/TrackMyRoute.java
@@ -0,0 +1,16 @@
+package com.marcostoral.keepmoving.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class TrackMyRoute extends Service {
+    public TrackMyRoute() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
