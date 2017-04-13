76821707942bbdaf0bddefe7b125c939107ca02e
mcartog <marcostoral@hotmail.com>
2017-04-13 23:24:35 +0200

    Dialogo asociado al boton empezar ruta

--- KeepMoving/app/src/main/java/com/marcostoral/keepmoving/activities/MainActivity.java
+++ KeepMoving/app/src/main/java/com/marcostoral/keepmoving/activities/MainActivity.java
@@ -1,6 +1,9 @@
 package com.marcostoral.keepmoving.activities;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -19,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     private Button btnHistory;
     private Button btnStadistics;
 
+    //Dialogs
+    private Dialog dialogRouteType;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         init();
     }
-
 
     ///////////////////////////////////////////////////////
     ////////////////////   MENUS   ////////////////////////
@@ -54,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
     ///////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////
@@ -64,13 +68,15 @@ public class MainActivity extends AppCompatActivity {
         btnStadistics = (Button) findViewById(R.id.btnStadistics);
         btnHistory = (Button) findViewById(R.id.btnHistory);
 
+        dialogRouteType = generateDialogRouteType();
+
 
         btnMaps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                dialogRouteType.show();
                 Toast.makeText(MainActivity.this, "al mapa",Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-                startActivity(intent);
+
             }
         });
 
@@ -92,7 +98,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
 
+    /**
+     * Crea el diálogo de selección de tipo de actividad.
+     * @return
+     */
+    private Dialog generateDialogRouteType(){
+
+        final String[] items = getResources().getStringArray(R.array.route_type);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle(R.string.title_dialog_route_type);
+        builder.setItems(R.array.route_type, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) {
+                Toast.makeText(MainActivity.this,"Opción elegida: " + items[item],Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                intent.putExtra("type",items[item]);
+                startActivity(intent);
+            }
+        });
+
+        return builder.create();
     }
 
 }
